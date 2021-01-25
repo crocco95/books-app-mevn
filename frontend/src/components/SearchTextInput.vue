fcc645bfee07baccfff77164f96cc1019f298c89
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-01-25 23:13:27 +0100

    Remove About page and Add Search page
    Remove autogenerated "About" page
    Add a simple Search skeleton page

---
+++ frontend/src/components/SearchTextInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Search</h1>
+    <input type="text" placeholder="Search something like 'Biancaneve', 'Divina Commedia', etc..."/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="search">
+    <SearchTextInput />
+  </div>
+</template>
+
+<script>
+
+import SearchTextInput from '@/components/SearchTextInput.vue'
+
+export default {
+  components: {
+    SearchTextInput
+  }
+}
+</script>
+
+<style>
+
+</style>

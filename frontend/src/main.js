380bf806e62e942c541d7897396ffe7ad6f955de
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-01-25 23:44:15 +0100

    Add Bootstrap 5 dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(bootstrap).use(router).mount('#app')

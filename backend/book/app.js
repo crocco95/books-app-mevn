e6305d4440d90ca46d7d7037a06aa86e7b745601
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-01-29 16:16:20 +0100

    Change app.js to load cors and body-parser

--- backend/book/app.js
+++ backend/book/app.js
@@ -1,8 +1,17 @@
+// Setting up Express, the NodeJS framework
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+// Setting up cors: it helps to solve cors policy problem when client calls API
+const cors = require('cors');
+app.use(cors());
 
+// Setting up body-parser: it permits to extract data from http request body
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Setting up application port
+const port = process.env.PORT || 3000;
 console.log(`Listening on port ${port}`);
 
 // Listen for connections

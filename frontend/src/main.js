65b6d993a1bcf8c7f5fe5c2eb8bec81704101fcb
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-01-31 16:20:05 +0100

    Add Vuex to manage state better and avoid giant components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import store from './store'
 
-createApp(App).use(bootstrap).use(router).mount('#app')
+createApp(App).use(store).use(bootstrap).use(router).mount('#app')

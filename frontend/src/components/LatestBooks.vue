3b3920b27699b5f16805f37eff6e72d6a2f2ee87
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-03 01:41:53 +0100

    Implement a first version of LatestBooks component
    It filter by category the books and order them by release date (newest).
    The category is passed by prop.

--- frontend/src/components/LatestBooks.vue
+++ frontend/src/components/LatestBooks.vue
@@ -1,25 +1,85 @@
 <template>
   <div class="latests-books">
-    <h2>LatestBooks</h2>
 
-    <BookPreviewCard class="col-md-3 col-sm-4 col-6" title="name" description="ciao" />
+    <div class="row">
+      <div class="col col-md-3 col-sm-4 col-6"
+        v-for="book in books"
+        :key="book.id">
+        
+        <BookPreviewCard
+        :id="book.id"
+        :coverUrl="book?.volumeInfo?.imageLinks?.thumbnail"
+        :title="book.volumeInfo.title"
+        :subtitle="book.volumeInfo.subtitle"
+        :description="book.volumeInfo.description"
+        :authors="book.volumeInfo.authors"/>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 
 import BookPreviewCard from './BookPreviewCard.vue';
+import axios from 'axios';
+import httpUtils from '../utils/http';
 
 export default {
   name: 'LatestBooks',
+
+  props: {
+    category: String
+  },
+
+  data(){
+    return {
+      books: []
+    }
+  },
+
   components: {
     BookPreviewCard
+  },
+
+  methods:{
+    fetchBooks(){
+
+      const params = {
+        query: 'subject:' + this.category,
+        orderBy: 'newest',
+      }
+
+      console.log(`http://localhost:3000/api/v1/books?` + httpUtils.paramsToUrlQuery(params));
+
+      axios
+        .get(`http://localhost:3000/api/v1/books?` + httpUtils.paramsToUrlQuery(params))
+        .then( res => {
+          console.log(res);
+          this.books = res.data.items;
+        })
+        .catch( err => {
+          console.error(err);
+        });
+    }
+  },
+
+  mounted(){
+    this.fetchBooks()
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .latests-books {
-    text-align: left;
-  }
+
+  /* The heart of the matter */
+.row {
+  display: block;
+  overflow-x: auto;
+  white-space: nowrap;
+}
+.row > .col {
+  display: inline-block;
+}
+
 </style>

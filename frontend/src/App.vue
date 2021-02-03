e1866be8a94730a5e66e0a23020d81f3228292f0
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-03 12:43:14 +0100

    Change navbar style from the Vue default to a Bootstrap sticky one

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/search">Search</router-link> |
-    <router-link to="/profile">My Profile</router-link>
-  </div>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <img alt="App logo" src="./assets/logo.png" width="30">
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/search">Search</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/profile">My Profile</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
   <router-view/>
 </template>
 

bd7c8aaf6a6e70b99903afd6b3a5dba3e2d229a7
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-03 16:18:21 +0100

    Add init firebase app sequence to main.js
    The firebase_sample.js is a sample config file for Firebase, the original one it's not committed for security reasons.

---
+++ frontend/src/config/firebase_sample.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "*****",
+  authDomain: "nevm-books-app-project.firebaseapp.com",
+  projectId: "nevm-books-app-project",
+  storageBucket: "nevm-books-app-project.appspot.com",
+  messagingSenderId: "*****",
+  appId: "*****"
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import store from './store'
+import firebase from 'firebase/app';
+
+// Init firebase
+import firebaseConfig from './config/firebase';
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(bootstrap).use(router).mount('#app')

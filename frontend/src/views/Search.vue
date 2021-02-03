49e60d524cd136913e06f108305799de55694a56
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-03 12:42:36 +0100

    Change search page style
    Now the page hase a background image behind the search bar and it acts as header. Nicer to look in general.

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="container">
-    <div class="row">
+  <div class="container-fluid">
+    <div class="row search-bar-container">
       <div class="col-md-8 offset-md-2">
-        <h1>Search</h1>
+        <h1 class="fw-bolder">Search</h1>
         <SearchTextInput />
+
+        <small class="tip text-secondary">
+          <strong>Tip:</strong> Are you searching for a person? Put the "@" in the search bar before her username!
+        </small>
       </div>
-      <small class="text-secondary">
-        <strong>Tip:</strong> Are you searching for a person? Put the "@" in the search bar before her username!
-      </small>
     </div>
+
 
     <div class="row mt-4">
       <div class="col-md-8 offset-md-2">
@@ -35,5 +37,20 @@ export default {
 <style scoped>
   h1{
     text-align: left;
+  }
+
+  .search-bar-container {
+    background-image: url('../assets/bg_search.jpg');
+    background-position: center;
+    background-size: cover;
+    /* background-attachment: fixed; */ /* Great visual impact but not so much usefull here... */
+
+    padding: 20vh 0;
+  }
+
+  .tip {
+    padding: 10px 20px;
+    border-radius: 100rem;
+    background-color: rgba(255, 255, 255, .8);
   }
 </style>

3e2fe288c477f804816c6bd2bbd30123b2e8a38e
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-03 01:42:54 +0100

    Change homepage layout adding some LatestBooks component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="container home">
     <img alt="App logo" src="../assets/logo.png" width="150">
-    <h1>Welcome to Books App!</h1>
+    <h1 class="text-left">Welcome to Books App!</h1>
     
-    <h2></h2>
-    <LatestBooks limit="12"/>
+    <h2 class="mt-5">Humor</h2>
+    <LatestBooks category="humor" limit="12"/>
 
-    <MostReadBooks limit="12"/>
+    <h2 class="mt-5">History</h2>
+    <LatestBooks category="history" limit="12"/>
+
+    <h2 class="mt-5">Travel</h2>
+    <LatestBooks category="travel" limit="12"/>
   </div>
 </template>
 

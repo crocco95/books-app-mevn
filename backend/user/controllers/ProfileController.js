917523cc414fd5672f1c1c4a35efd1b80a3e8ac5
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-06 20:18:45 +0100

    Add user id extraction from token via Firebase Admin SDK

--- backend/user/controllers/ProfileController.js
+++ backend/user/controllers/ProfileController.js
@@ -1,17 +1,26 @@
+const firebaseAdmin = require('firebase-admin');
 const pick = require('../utils/pick');
 const profileService = require('../services/ProfileService');
 
 const create = async ( req, res ) => {
-  
+
   let params = {
-    uid: req.body.uid,
     surname: req.body.surname,
     name: req.body.name,
     genre: req.body.genre,
   }
 
-  profileService
-    .add( params.uid, params.name, params.surname, params.genre )
+  const idToken = req.headers.authorization;
+  if( !idToken ){
+    res.status(401).json({message: "Not valid Token"});
+    return;
+  }
+
+  firebaseAdmin
+    .auth()
+    .verifyIdToken( idToken )
+    .then( decodedToken => params.uid = decodedToken.uid )
+    .then( () => profileService.add( params.uid, params.name, params.surname, params.genre ))
     .then( profile => res.status( 201 ).json( profile ))
     .catch( err => res.status( 400 ).json( err ));
 };

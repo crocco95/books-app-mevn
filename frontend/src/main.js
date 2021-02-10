8d03ad9145bdf4923ff7c4ee9ed18f2695236890
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-10 10:11:51 +0100

    Add import Bootstrap JS file
    Without Bootstrap JS components like modals won't work

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrapCss from 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrapJs from 'bootstrap/dist/js/bootstrap'
 import store from './store'
 import firebase from 'firebase/app';
 
@@ -10,4 +11,4 @@ import firebase from 'firebase/app';
 import firebaseConfig from './config/firebase';
 firebase.initializeApp(firebaseConfig);
 
-createApp(App).use(store).use(bootstrap).use(router).mount('#app')
+createApp(App).use(store).use(bootstrapCss).use(bootstrapJs).use(router).mount('#app')

a5ce278e8566abda7b4f951da77c2c0da4dee989
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-10 12:02:59 +0100

    Add LongText component to manage long paragraphs
    This component want to manage long text blocks adding a simple "Show more" button to toggle the showing text cut after a number of chars

---
+++ frontend/src/components/LongText.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p v-html="shortText" class="text"></p>
+    <div class="text-center">
+      <button class="btn btn-link" @click="showMore">Read {{ moreFlag ? 'less' : 'more' }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props:{
+    text: String,
+    max: Number,
+  },
+
+  data(){
+    return {
+      shortText: '',
+      moreFlag: true,
+    }
+  },
+
+  methods:{
+    showMore(){
+      this.moreFlag = !this.moreFlag;
+      this.shortText = this.moreFlag ? this.text : this.text.substr(0,this.max) + '...';
+    }
+  },
+
+  watch: {
+    text: [{
+      handler: 'showMore'
+    }]
+  },
+
+  mouted(){
+    this.moreFlag = true;
+    this.showMore()
+  }
+
+}
+</script>
+
+<style scoped>
+  .text{
+    transition: all .2s;
+  }
+</style>

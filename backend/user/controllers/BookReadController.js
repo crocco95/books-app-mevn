9c2d7a110abe068862e0ca6724a4230f15e23783
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-17 17:56:40 +0100

    Remove useless token verify in BookReadController add function

--- backend/user/controllers/BookReadController.js
+++ backend/user/controllers/BookReadController.js
@@ -5,18 +5,23 @@ const bookReadService = require('../services/BookReadService');
 
 const add = ( req, res ) => {
 
-  firebaseAdmin
-    .auth()
-    .verifyIdToken( idToken )
-    .then( decodedToken => 
-      bookReadService
-        .add(
-        decodedToken.uid,
-        req.body.bookId,
-        req.body.currentPage,
-        req.body.startDate,
-        req.body.finishDate,
-      )
+  const userId = req.body.tokenUserId;
+
+  if( !userId ){
+    res.status(403).json({
+      message: 'You are not allowed',
+      code: 403,
+    });
+    return;
+  }
+
+  bookReadService
+    .add(
+      userId,
+      req.body.bookId,
+      req.body.currentPage,
+      req.body.startDate,
+      req.body.finishDate,
     )
     .then( rd => res.status(201).json(rd) )
     .catch( err => res.status(400).json(err));

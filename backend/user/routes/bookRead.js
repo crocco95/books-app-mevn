4465420c1ce7eefba1d36017fa715deedfa2ab1e
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-17 18:06:32 +0100

    Change the "me" word to user ID param
    The ID param is essentially useless but help to track the user who did the request

--- backend/user/routes/bookRead.js
+++ backend/user/routes/bookRead.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const BookReadController = require('../controllers/BookReadController');
 
 router
-  .route('/me/books')
-    .post(BookReadController.add);
+  .route('/:userId/books')
+  .post(BookReadController.add);
 
 module.exports = router;

65209ff422b3339eaea4dd8ae8ef805d65747891
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-17 18:09:21 +0100

    Remove service worker import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import bootstrapCss from 'bootstrap/dist/css/bootstrap.min.css'
 import bootstrapJs from 'bootstrap/dist/js/bootstrap'

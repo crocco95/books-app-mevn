1c838f154b3e101cdc243e48bb8e86e192da3b98
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-24 18:51:46 +0100

    Add indexes in BookReadSchema to optimize queries

--- backend/user/models/bookRead.js
+++ backend/user/models/bookRead.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const BookReadSchema = mongoose.Schema({
   userId: {
     type: String,
+    index: true,
     required: true
   },
   bookId: {
     type: String,
+    index: true,
     required: true
   },
   currentPage: {

fcd9eb0e237b314b6fba877929f1d5d1471bca3f
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-02-24 17:23:59 +0100

    Add some routes to gateway and the cors policy middleware

--- backend/gateway/app.js
+++ backend/gateway/app.js
@@ -2,15 +2,28 @@ const PORT = process.env.PORT || 8080;
 
 const gateway = require('fast-gateway');
 const server = gateway({
+  middlewares: [
+    require('cors')()
+  ],
   routes: [
+    {
+      prefix: '/api/v1/books',
+      prefixRewrite: '/api/v1/books',
+      target: 'http://books:3000'
+    },
     {
       prefix: '/api/v1/books/',
       prefixRewrite: '/api/v1/books/',
       target: 'http://books:3000'
     },
     {
-      prefix: '/api/v1/users/',
-      prefixRewrite: '/api/v1/users/',
+      prefix: '/api/v1/users',
+      prefixRewrite: '/api/v1/users',
+      target: 'http://users:3000'
+    },
+    {
+      prefix: '/api/v1/profiles',
+      prefixRewrite: '/api/v1/profiles',
       target: 'http://users:3000'
     },
     {

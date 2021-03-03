f176bdf040025b46e341e0b254ae0046d7e77afe
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-03-03 10:55:38 +0100

    Add comments to SocialService fucntions

--- backend/user/services/SocialService.js
+++ backend/user/services/SocialService.js
@@ -1,5 +1,12 @@
 const social = require('../models/social');
 
+/**
+ * List all users follow an user and all users the user follow.
+ * The function return an object with 2 array attributes called "following" and "followers".
+ * The arrays contain the social objects NOT the user objects.
+ * The user subject is identified by userId param.
+ * @param {String} userId 
+ */
 const list = async ( userId ) => {
 
   const f = {
@@ -14,10 +21,20 @@ const list = async ( userId ) => {
     .then( () => f);
 };
 
+/**
+ * Retrieve a specific social relation of type "a user identified by userId follow the user identified by followingUserId"
+ * @param {String} userId 
+ * @param {String} followingUserId 
+ */
 const get = async ( userId, followingUserId ) => {
   return social.findOne({userId, followingUserId});
 };
 
+/**
+ * Add a new social relation of kind: a user identified by userId follow another user identified by followingUserId
+ * @param {String} userId 
+ * @param {String} followingUserId 
+ */
 const add = async ( userId, followingUserId ) => {
 
   return social
@@ -32,6 +49,11 @@ const add = async ( userId, followingUserId ) => {
     .then(() => social.create({userId, followingUserId}));
 };
 
+/**
+ * Remove a social relation
+ * @param {String} userId 
+ * @param {String} followingUserId 
+ */
 const remove = async ( userId, followingUserId ) => {
   return social.deleteOne({userId, followingUserId});
 };

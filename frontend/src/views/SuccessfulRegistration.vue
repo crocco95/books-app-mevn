479835a0ab6041884de1ebbe4b1127a9ad28df66
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-03-21 20:06:22 +0100

    Change registration success page to match email verification update
    Instead of ask to the user for preferred genres, the app tells to user to check email address for the verification email.
    The preferred genres will be extrapolated from the user's books.

--- frontend/src/views/SuccessfulRegistration.vue
+++ frontend/src/views/SuccessfulRegistration.vue
@@ -2,52 +2,26 @@
   <div class="container">
     <div class="row mt-5">
       <div class="col-md-8 offset-md-2">
-        <h1>WELCOME IN!</h1>
-        
         <div class="alert alert-success">
-          <strong>It's done!</strong> <span>Registration completed successfully!</span>
+          <strong>🥳 Success!</strong> <span>Registration is almost complete!</span>
         </div>
       </div>
     </div>
 
-    <div class="row mt-3">
-      <div class="col-md-12">
-        <h3>What's your favourites book's categories ?</h3>
-        <p>Choose almost 5 genre in order to let us know what can we show you</p>
+    <!-- START info text -->
+    <div class="row mt-1">
+      <div class="col-md-8 offset-md-2">
+        <p>
+          A confirmation email is sent to your email address, go to click the link inside to verify it and enable the login.
+        </p>
       </div>
     </div>
-
-    <div class="row mt-3">
-      <div class="col-md-3 card-genre">
-        <div class="category-emoji">❤️‍🔥</div>
-        <h3>LOVE</h3>
-        <p>The category of books such as:</p>
-        <input type="checkbox" name="love" />
-      </div>
-      <div class="col-md-3 card-genre">
-        <div class="category-emoji">😂</div>
-        <h3>HUMOR</h3>
-        <p>The category of books such as:</p>
-        <input type="checkbox" name="love" />
-      </div>
-      <div class="col-md-3 card-genre">
-        <div class="category-emoji">🤔</div>
-        <h3>INSPIRATIONAL</h3>
-        <p>The category of books such as:</p>
-        <input type="checkbox" name="love" />
-      </div>
-      <div class="col-md-3 card-genre">
-        <div class="category-emoji">🧙🐲</div>
-        <h3>FANTASY</h3>
-        <p>The category of books such as:</p>
-        <input type="checkbox" name="love" />
-      </div>
-    </div>
+    <!-- END info text -->
 
     <!-- START Continue button -->
-    <div class="row mt-5">
+    <div class="row mt-1">
       <div class="col-md-12">
-        <button class="btn btn-primary btn-lg px-5">That's it, continue</button>
+        <router-link class="btn btn-primary px-5" to="/login">Continue to login</router-link>
       </div>
     </div>
     <!-- END Continue button -->
---
+++ frontend/src/views/SuccessfulRegistration.vue.old
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-8 offset-md-2">
+        <h1>WELCOME IN!</h1>
+        
+        <div class="alert alert-success">
+          <strong>It's done!</strong> <span>Registration completed successfully!</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col-md-12">
+        <h3>What's your favourites book's categories ?</h3>
+        <p>Choose almost 5 genre in order to let us know what can we show you</p>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col-md-3 card-genre">
+        <div class="category-emoji">❤️‍🔥</div>
+        <h3>LOVE</h3>
+        <p>The category of books such as:</p>
+        <input type="checkbox" name="love" />
+      </div>
+      <div class="col-md-3 card-genre">
+        <div class="category-emoji">😂</div>
+        <h3>HUMOR</h3>
+        <p>The category of books such as:</p>
+        <input type="checkbox" name="love" />
+      </div>
+      <div class="col-md-3 card-genre">
+        <div class="category-emoji">🤔</div>
+        <h3>INSPIRATIONAL</h3>
+        <p>The category of books such as:</p>
+        <input type="checkbox" name="love" />
+      </div>
+      <div class="col-md-3 card-genre">
+        <div class="category-emoji">🧙🐲</div>
+        <h3>FANTASY</h3>
+        <p>The category of books such as:</p>
+        <input type="checkbox" name="love" />
+      </div>
+    </div>
+
+    <!-- START Continue button -->
+    <div class="row mt-5">
+      <div class="col-md-12">
+        <button class="btn btn-primary btn-lg px-5">That's it, continue</button>
+      </div>
+    </div>
+    <!-- END Continue button -->
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .category-emoji{
+    font-size: 5rem;
+  }
+</style>

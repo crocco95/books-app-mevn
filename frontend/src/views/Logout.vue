e755b30b11b8f4897e2411d1aa552130d68034d4
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-06-06 16:10:24 +0200

    Change Logout page to match new auth methods and minor changes to UI

--- frontend/src/views/Logout.vue
+++ frontend/src/views/Logout.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
     <div class="mt-5">
+      <span class="h1">👋</span>
       <h1>Logout done!</h1>
       <h4 class="text-muted">We hope to see you soon again</h4>
     </div>
     <div class="mt-3">
-      <router-link to="/" class="mx-2 btn btn-primary">Return to Homepage</router-link>
+      <router-link to="/" class="mx-2 btn btn-link">Return to Homepage</router-link>
       <router-link to="/login" class="mx-2 btn btn-primary">Go to Login page</router-link>
     </div>
   </div>
 </template>
 
 <script>
+
+import {mapActions} from 'vuex';
+
 export default {
+
+  methods:{
+    ...mapActions(['logout']),
+  },
+
   mounted(){
-    window.localStorage.removeItem('_token');
-    window.localStorage.removeItem('_userId');
+    this.logout();
   }
 }
 </script>

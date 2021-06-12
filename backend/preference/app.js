b546c59ebc0d7e00dc4e6ac9f02b3450477dea43
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-06-12 16:16:54 +0200

    Add Http listener belong to AMQP on Preferences service

--- backend/preference/app.js
+++ backend/preference/app.js
@@ -22,5 +22,15 @@ try{
   console.log({error: err});
 }
 
+const routes = require('./routes');
+app.use('/api/v1',routes);
+
+// Setting up application port
+const port = process.env.PORT || 3000;
+console.log(`Listening on port ${port}`);
+
+// Listen for connections
+app.listen(port);
+
 const amqpController = require('./controllers/AmqpController');
 amqpController.connect();

d0db32b3ece668738975007951523200deff0595
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-06-21 22:47:10 +0200

    Add real slides and fix issues on HomePreferenceHero

--- frontend/src/components/HomePreferenceHero.vue
+++ frontend/src/components/HomePreferenceHero.vue
@@ -6,18 +6,23 @@
         v-for="slide in slides" :key="slide.id"
         type="button"
         data-bs-target="#home-hero-carousel"
-        data-bs-slide-to="0"
-        class="active"
+        :data-bs-slide-to="slide.id"
+        :class="slide.id <= 0 ? 'active' : ''"
         aria-current="true"
-        aria-label="Slide 1"></button>
+        :aria-label="`Slide ${slide.id}`"></button>
     </div>
     <div class="carousel-inner">
       
-      <div class="carousel-item active" v-for="slide in slides" :key="slide.id">
-        <img src="" class="d-block w-100" alt="...">
+      <div :class="'carousel-item ' + (slide.id <= 0 ? 'active' : '')" v-for="slide in slides" :key="slide.id">
+        <img :src="slide.imageUri" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
+          <h1 class="text-uppercase fw-bolder" v-text="slide.title"></h1>
+          <p v-text="slide.description"></p>
+          <router-link  class="btn btn-primary"
+            v-for="button in slide.buttons" :key="button.id"
+            v-text="button.title"
+            :to="button.uri"
+            v-on:click="prepareSearch(slide.title)"></router-link>
         </div>
       </div>
       
@@ -34,7 +39,7 @@
 </template>
 <script>
 
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import axios from 'axios';
 
 export default {
@@ -48,19 +53,58 @@ export default {
   methods:{
 
     ...mapGetters(['getUser']),
+    ...mapActions(['fetchBooks']),
 
     getPreferences(){
 
       const user = this.getUser();
 
-      axios
+      return axios
         .get(`http://localhost:8080/api/v1/users/${user.uid}/preferences`)
+        .then( res => res.data )
+    },
+
+    prepareSearch(category){
+      this.fetchBooks({
+        query: `subject=${category}&orderBy=newest`,
+        limit: 12,
+      }).then( () => {
+        this.searchingFlag = false;
+      });
     }
   },
 
-  mounted() {
-    const user = this.getPreferences();
-    console.log(user);
+  async mounted(){
+    const preferences = await this.getPreferences();
+    
+    for(let i = 0 ; i < 4 && i < Object.keys(preferences.categories).length; i++ ){
+
+      let category = Object.keys(preferences.categories)[i];
+
+      this.slides.push({
+        id: i,
+        title: category,
+        description: "Explore your preferences, here some ideas for you!",
+        imageUri: `https://source.unsplash.com/1920x1080/?${category}`,
+        buttons: [
+          {
+            id: 0,
+            title: '🔝 Discover the newest releases',
+            uri: '/search',
+          },
+        ]
+      });
+    }
   }
 }
 </script>
+<style scoped lang="scss">
+
+  .carousel-caption{
+    background-color: rgba(0,0,0,.4);
+    padding: 2rem 1rem;
+  }
+  .carousel-item{
+    max-height: 80vh;
+  }
+</style>

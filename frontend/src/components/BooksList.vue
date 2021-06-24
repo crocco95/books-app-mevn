803d4d57f22055ede03739f954f5cda5c5cf8fe4
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-06-24 18:37:25 +0200

    Add an allert for no search

--- frontend/src/components/BooksList.vue
+++ frontend/src/components/BooksList.vue
@@ -1,4 +1,13 @@
 <template>
+
+  <div class="row" v-if="!listBooks.items">
+    <div class="col-md-12">
+      <div class="alert alert-info">
+        <strong>Hei!</strong> Write the title of the book you are searching for or part of it inside the search bar to start
+      </div>
+    </div>
+  </div>
+
   <div class="row">
     <div class="col-md-4 col-sm-6 col-6 mb-4" v-for="book in listBooks.items" :key="book.id">
 

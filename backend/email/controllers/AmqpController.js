ca638d8c1d6f44d6e26345ba73ab01a02ed1d605
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-07-01 15:33:44 +0200

    Refactor AmqpController

--- backend/email/controllers/AmqpController.js
+++ backend/email/controllers/AmqpController.js
@@ -1,31 +1,41 @@
+const amqp = require('amqplib');
 const emailService = require('../services/EmailService');
 
 const connect = () => {
-  const amqp = require('amqplib');
 
-  amqp.connect(process.env.AMQP_BROKER_URI).then(function(conn) {
-    process.once('SIGINT', function() { conn.close(); });
-    return conn.createChannel().then(function(ch) {
+  // Connect to AMQP broker
+  amqp
+    .connect(process.env.AMQP_BROKER_URI)
+    .then(function(conn) {
+      process.once('SIGINT', function() { conn.close(); });
+      return conn.createChannel().then(function(ch) {
 
-      let ok = ch.assertQueue('send-notification', {durable: true});
+        let ok = ch.assertQueue(process.env.AMQP_NOTIFICATION_TOPIC, {durable: true});
 
-      ok = ok.then(function(_qok) {
-        return ch.consume('send-notification', function(msg) {
-          console.log(" [x] Received '%s'", msg.content.toString());
-          const message = JSON.parse(msg.content.toString());
+        ok = ok.then(function(_qok) {
+          return ch.consume(process.env.AMQP_NOTIFICATION_TOPIC, function(msg) {
+            console.log("[AMQP] Received '%s'", msg.content.toString());
+            const message = JSON.parse(msg.content.toString());
 
-          emailService.send(message.target, message.subject, message.textBody, message.htmlBody)
-          .then(p => console.log(`Done : ${JSON.stringify(p)}`))
-          .catch(err => console.error(err));
-          
-        }, {noAck: true});
+            // Check if the message packet has the required fields
+            if(message.target && message.subject && message.textBody && message.htmlBody){
+
+              // Call the send function on EmailService
+              emailService
+                  .send(message.target, message.subject, message.textBody, message.htmlBody)
+                  .then(p => console.log(`[AMQP] Done : ${JSON.stringify(p)}`))
+                  .catch(err => console.error(err));
+            }
+
+          }, {noAck: true});
+        });
+
+        return ok.then(function(_consumeOk) {
+          console.log('[AMQP] Waiting for messages.');
+        });
       });
-
-      return ok.then(function(_consumeOk) {
-        console.log(' [*] Waiting for messages.');
-      });
-    });
-  }).catch(console.warn);
+    })
+    .catch(console.warn);
 }
 
 module.exports = {

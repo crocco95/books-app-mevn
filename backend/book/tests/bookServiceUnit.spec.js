9ad47827de3e00a7ba9d176543a115a86c0459e2
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-07-09 16:35:39 +0200

    Add unit test for BookService

---
+++ backend/book/tests/bookServiceUnit.spec.js
@@ -0,0 +1,23 @@
+const bookService = require('../services/BookService');
+
+describe('Search a list of books by genre using whe Google Books API', () => {
+    test('List all books by searching Harry Potter', async () => {
+        const result = await bookService.list({
+            query: 'subject:travel',
+        });
+
+        expect(result.status).toBe(200);
+    });
+
+    test('Searching a book by its unique ID', async () => {
+
+        const volumeId = '9CJWTbd-RYoC';
+
+        const result = await bookService.get(volumeId);
+
+        expect(result.status).toBe(200);
+        expect(result.data?.id).toBe(volumeId);
+        expect(result.data?.volumeInfo?.title).toBe('Harry Potter e la Pietra Filosofale');
+    })
+
+});

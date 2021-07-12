31213fdaeab9269ff604cfa34072c9a46188d45c
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-07-12 10:32:16 +0200

    Add tests for UserService functions

---
+++ backend/user/test/userServiceUnit.spec.js
@@ -0,0 +1,17 @@
+const userService = require('../services/UserService');
+const userId = 'yvphy03pftV0qCMxRXzcUnocC912';
+
+beforeAll(() => {
+    // Setting up Firebase Admin SDK
+    const firebaseAdmin = require('firebase-admin');
+    const serviceAccount = require("../config/serviceAccountKey.json");
+
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(serviceAccount)
+    });
+});
+
+test('Test retrieve a specific user', async () => {
+    const user = await userService.get(userId);
+    expect(user.uid).toBe(userId);
+});

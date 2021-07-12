45c47b9733684872d129c2c87d6416226cabdec1
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-07-12 16:02:05 +0200

    Add list function and a check for profile existence before to try to create on ProfileService

--- backend/user/services/ProfileService.js
+++ backend/user/services/ProfileService.js
@@ -1,8 +1,21 @@
 const Profile = require('../models/profile');
 
+const list = () => {
+  return Profile.find();
+}
+
 const add = ( profile ) => {
   console.log(`Adding profile: ${JSON.stringify(profile)}`);
-  return Profile.create(profile);
+  return Profile
+      .findOne({
+        _id: profile._id
+      })
+      .then(result => {
+        if(result){
+          throw new TypeError(`The profile ${profile._id} already exists`);
+        }
+      })
+      .then(() => Profile.create(profile));
 }
 
 const get = ( userId ) => {
@@ -22,8 +35,16 @@ const edit = (userId, params) => {
     });
 }
 
+const remove = (userId) => {
+  return Profile.deleteOne({
+    _id: userId
+  });
+}
+
 module.exports = {
+  list,
   add,
   get,
   edit,
+  remove,
 }

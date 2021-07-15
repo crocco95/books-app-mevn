eb5c55492edd8a692109222e5f29a4e6dbceffbf
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-07-15 20:44:24 +0200

    Add Load more button on search screen

--- frontend/src/components/BooksList.vue
+++ frontend/src/components/BooksList.vue
@@ -1,45 +1,60 @@
 <template>
 
-  <div class="row" v-if="!listBooks.items">
-    <div class="col-md-12">
-      <div class="alert alert-info">
-        <strong>Hei!</strong> Write the title of the book you are searching for or part of it inside the search bar to start
-      </div>
+    <div class='row' v-if='!getBooks().items'>
+        <div class='col-md-12'>
+            <div class='alert alert-info'>
+                <strong>Hei!</strong> Write the title of the book you are searching for or part of it inside the search
+                bar to start
+            </div>
+        </div>
     </div>
-  </div>
 
-  <div class="row">
-    <div
-    class="col-md-4 col-sm-6 col-6 mb-4"
-    v-for="book in listBooks.items"
-    :key="book.id">
+    <div class='row'>
+        <div
+            class='col-md-4 col-sm-6 col-6 mb-4'
+            v-for='book in getBooks().items'
+            :key='book.id'>
 
-      <BookPreviewCard
-        :id="book.id"
-        :coverUrl="book?.volumeInfo?.imageLinks?.thumbnail"
-        :title="book.volumeInfo.title"
-        :subtitle="book.volumeInfo.subtitle"
-        :description="book.volumeInfo.description"
-        :authors="book.volumeInfo.authors"/>
+            <BookPreviewCard
+                :id='book.id'
+                :coverUrl='book?.volumeInfo?.imageLinks?.thumbnail'
+                :title='book.volumeInfo.title'
+                :subtitle='book.volumeInfo.subtitle'
+                :description='book.volumeInfo.description'
+                :authors='book.volumeInfo.authors' />
+        </div>
 
+        <div class='row mt-4 pb-5' v-if='!areBooksFinished()'>
+            <div class='col-md-8 offset-md-2'>
+                <button
+                    class='btn btn-outline-primary'
+                    v-on:click='loadMore()'>
+                    Load more results
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex'
 import BookPreviewCard from './BookPreviewCard.vue'
 
 export default {
-  components: { BookPreviewCard },
-  
-  computed: {
-    ...mapGetters(['listBooks'])
-  },
+    components: { BookPreviewCard },
+
+    methods: {
+
+        ...mapGetters(['getBooks', 'getLastSearch', 'areBooksFinished']),
+        ...mapActions(['loadMoreBooks']),
+
+        async loadMore() {
+            const books = await this.getBooks();
+            const lastSearch = await this.getLastSearch();
+
+            await this.loadMoreBooks({ books, lastSearch });
+        }
+    },
 }
 </script>
-
-<style>
-
-</style>
--- frontend/src/store/modules/books.js
+++ frontend/src/store/modules/books.js
@@ -2,30 +2,71 @@ import axios from 'axios';
 import httpUtils from '../../utils/http';
 
 const state = {
+  lastSearch: '',
   books: [],
+  booksFinishedFlag: false,
 }
 
 const getters = {
-  listBooks: (state) => state.books,
+  getBooks: (state) => state.books,
+  getLastSearch: (state) => state.lastSearch,
+  areBooksFinished: (state) => state.booksFinishedFlag,
 }
 
 const mutations = {
   setBooks: (state,books) => ( state.books = books ),
+  setLastSearch: (state,search) => ( state.lastSearch = search ),
+  setAreBooksFinishedFlag: (state,booksFinishedFlag) => ( state.booksFinishedFlag = booksFinishedFlag ),
 }
 
 const actions = {
   async fetchBooks({ commit }, params) {
 
+    commit('setLastSearch', params);
     const str = httpUtils.paramsToUrlQuery(params);
 
     return axios
-      .get('/books?' + str,{
-        method: 'get'
-      })
-      .then( res => commit('setBooks', res.data) );
+        .get('/books?' + str)
+        .then( res => {
+
+          commit('setBooks', res.data)
+          commit('setAreBooksFinishedFlag', params.limit >= res.data.totalItems)
+
+        });
+  },
+
+  async loadMoreBooks({commit}, params){
+
+    const lastSearch = params.lastSearch;
+    let books = params.books;
+
+    if (lastSearch && books) {
+
+      let newSearch = {
+        query: lastSearch.query,
+        limit: lastSearch.limit,
+        language: lastSearch.language ?? '',
+        startIndex: lastSearch.startIndex
+            ? lastSearch.startIndex + lastSearch.limit
+            : lastSearch.limit,
+      };
+
+      const str = httpUtils.paramsToUrlQuery(newSearch);
+      return axios
+          .get('/books?' + str,)
+          .then( res =>  {
+
+            books.items = books.items.concat(res.data.items);
+
+            commit('setLastSearch', newSearch);
+            commit('setBooks', books);
+
+            commit('setAreBooksFinishedFlag',
+                (newSearch.startIndex + newSearch.limit) >= books.totalItems)
+          });
+    }
   }
 }
-
 
 export default {
   state,
